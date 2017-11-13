e23ec9aecea6541396012a077fa71b109d6c32c5
ern19 <aarontrammell19@gmail.com>
2017-11-13 12:05:18 -0500

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {Route, BrowserRouter as Router, Switch, Redirect} from 'react-router-dom'
+import {Route, BrowserRouter as Router, Switch} from 'react-router-dom'
 import SignUpLogIn from './components/SignUpLogIn'
 import axios from 'axios'
 import {saveAuthTokens, setAxiosDefaults, parentIsLoggedIn, clearAuthTokens} from "./util/SessionHeaderUtil";
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom'
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 import FlatButton from 'material-ui/FlatButton';
-import Toggle from 'material-ui/Toggle';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
-import NavigationClose from 'material-ui/svg-icons/navigation/close';
 
 class Login extends Component {
 
@@ -21,8 +18,13 @@ class Login extends Component {
     }
 }
 
+const backToParent = () => {
+    
+}
+
 const Logged = (props) => (
     
+
     <IconMenu
         
         {...props}
