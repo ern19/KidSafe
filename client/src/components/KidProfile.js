09f7f00847c552b1cac96e2d21b426443017c713
ern19 <aarontrammell19@gmail.com>
2017-11-12 21:28:19 -0500

    made skeleton view for single kid profile

--- client/src/components/KidProfile.js
+++ client/src/components/KidProfile.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
 class KidProfile extends Component {
+
+    state ={
+        kid: {}
+    }
+
+    async componentWillMount() {
+        try {
+            const res = await axios.get(`/kids/${this.props.match.params.id}`)
+            this.setState({kid: res.data})
+            console.log(this.props.match)
+            console.log(res.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
     render() {
         return (
             <div>
-                this is timmy's profile?
+                {this.state.kid.nickname}
             </div>
         );
     }
