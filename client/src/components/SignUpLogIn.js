cc55ac7513b7a8bdfad7ebcad9d11f8a08393f2e
ern19 <aarontrammell19@gmail.com>
2017-11-14 16:26:47 -0500

    my handleChanges are really confused

--- client/src/components/SignUpLogIn.js
+++ client/src/components/SignUpLogIn.js
@@ -4,54 +4,78 @@ import TextField from 'material-ui/TextField'
 class SignUpLogIn extends Component {
 
     state = {
-        email: '',
-        password: '',
-        password_confirmation: ''
+        login: {
+            email: '',
+            password: '',
+        },
+        newAccount: {
+            email: '',
+            password: '',
+            password_confirmation: ''
+        }
     }
 
     signUp = (event) => {
         event.preventDefault()
         this.props.signUp(
-            this.state.email,
-            this.state.password,
-            this.state.password_confirmation
+            this.state.newAccount.email,
+            this.state.newAccount.password,
+            this.state.newAccount.password_confirmation
         )
     }
 
     signIn = (event) => {
         event.preventDefault()
         this.props.signIn(
-            this.state.email,
-            this.state.password
+            this.state.login.email,
+            this.state.login.password
         )
     }
 
-    handleChange = (event) => {
-        const newState = {...this.state}
+    handleLoginChange = (event) => {
+        const newState = {...this.state.login}
+        newState[event.target.name] = event.target.value
+        this.setState(newState)
+        
+    }
+
+    handleNewAccountChange = (event) => {
+        const newState = {...this.state.newAccount}
         newState[event.target.name] = event.target.value
         this.setState(newState)
     }
 
     render() {
         return (
-            <div>
+            <div style={{display: 'flex',
+                         justifyContent: 'space-between'}}>
                 <form>
                     <div>
                         <label htmlFor="email">E-mail: </label>
-                        <TextField onChange={this.handleChange} type="text" name="email" value={this.state.email}/>
+                        <TextField onChange={this.handleChange} type="text" name="email" value={this.state.login.email}/>
                     </div>
                     <div>
                         <label htmlFor="password">Password: </label>
-                        <TextField onChange={this.handleChange} type="password" name="password" value={this.state.password}/>
+                        <TextField onChange={this.handleChange} type="password" name="password" value={this.state.login.password}/>
+                    </div>
+                    <button onClick={this.signIn}>Log In</button>
+                    
+                </form>
+                <form>
+                    <div>
+                        <label htmlFor="email">E-mail: </label>
+                        <TextField onChange={this.handleChange} type="text" name="email" value={this.state.newAccount.email}/>
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password: </label>
+                        <TextField onChange={this.handleChange} type="password" name="password" value={this.state.newAccount.password}/>
                     </div>
                     <div>
                         <label htmlFor="password_confirmation">Confirm Password: </label>
                         <TextField onChange={this.handleChange} type="password" name="password_confirmation"
-                               value={this.state.password_confirmation}/>
+                               value={this.state.newAccount.password_confirmation}/>
                     </div>
-
                     <button onClick={this.signUp}>Sign Up</button>
-                    <button onClick={this.signIn}>Log In</button>
                 </form>
                 {this.props.amISignedIn ? <Redirect to="/kids"/> : <Redirect to="/signUp"/>}
                 {this.props.error ? <div style={{color:'red'}}>We don't recognize that username or password</div> : null}
