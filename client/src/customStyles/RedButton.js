18bc63fec226ee030fbbcf3017101124305f9f18
ern19 <aarontrammell19@gmail.com>
2017-11-16 12:19:15 -0500

    starting to clean up styled buttons and inputs

---
+++ client/src/customStyles/RedButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton'
+
+const RedButton = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default RedButton;
