1b6e67f4a551722e43c03b4c31ddfd990b37429c
ern19 <aarontrammell19@gmail.com>
2017-11-17 11:33:17 -0500

    changed href link in navbar to react-router link

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
@@ -28,7 +29,7 @@ const Logged = (props) => (
         targetOrigin={{horizontal: 'right', vertical: 'top'}}
         anchorOrigin={{horizontal: 'right', vertical: 'top'}}
     >
-        <MenuItem primaryText="Parent Profile" href='/kids'/>
+        <MenuItem primaryText="Parent Profile" containerElement={<Link to={'/kids'}/>}/>
         <MenuItem primaryText="Log Out" onClick={props.signOut}/>
     </IconMenu>
 )
@@ -44,6 +45,7 @@ class NavBar extends Component {
             <div>
                 <AppBar
                     style={{backgroundColor: '#bb0000'}}
+                    titleStyle={{fontSize: '2.2em'}}
                     title="KidSafe"
                     showMenuIconButton={false}
                     iconElementRight={this.props.amISignedIn ? <Logged signOut={this.props.signOut}/> : <Login />}
