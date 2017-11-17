2c7dbb5ef5f416476bc4211433e8be3521d305d6
ern19 <aarontrammell19@gmail.com>
2017-11-16 20:26:49 -0500

    replaced delete playlist button with svg icon

--- client/src/components/PlaylistList.js
+++ client/src/components/PlaylistList.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import axios from 'axios'
 import RaisedButton from 'material-ui/RaisedButton'
+import Delete from 'material-ui/svg-icons/action/delete-forever.js'
 
 class PlaylistList extends Component {
     deletePlaylist = async(playlistId) => {
@@ -19,11 +20,11 @@ class PlaylistList extends Component {
             <div>
                 {
                     this.props.playlists.map((playlist) => {
-                        const modified_URL = playlist.embed_URL + '?rel=0&amp;?&autoplay=0?&controls=0&?&showinfo=0?&modestbranding=1'
+                        const modified_URL = playlist.embed_URL + '?rel=0&amp;?&autoplay=0&?&showinfo=0?&modestbranding=1'
                         
                         return(
                             <div key={playlist.id}>
-
+                                
                                 <iframe src={modified_URL}
                                     width="500" 
                                     height="300"
@@ -34,9 +35,8 @@ class PlaylistList extends Component {
                                     title="youtube playlist"
                                     >           
                                 </iframe>
-                                <div>
-                                    <RaisedButton onClick={() => this.deletePlaylist(playlist.id)}backgroundColor="#bb0000" labelColor='#fff' style={{margin: '5px'}} label='Delete Playlist'/>
-                                </div>
+                                <div><Delete onClick={() => this.deletePlaylist(playlist.id)}/></div>
+                                
                                 
                             </div>
                     )})
@@ -47,3 +47,7 @@ class PlaylistList extends Component {
 }
 
 export default PlaylistList;
+
+{/* <div>
+    <RaisedButton onClick={() => this.deletePlaylist(playlist.id)}backgroundColor="#bb0000" labelColor='#fff' style={{margin: '5px'}} label='Delete Playlist'/>
+</div> */}
